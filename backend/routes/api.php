ac1df35ce8e30579ac4954008bdf2e9ee3fea419
udinesia325 <udinesia325@gmail.com>
2023-05-02 19:20:40 +0700

    create endpoint for dashboard

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\DashboardController;
 use App\Http\Controllers\Api\HistoriesController;
 use App\Http\Controllers\Api\InvoicesController;
 use App\Http\Controllers\Api\ProductController;
@@ -36,7 +37,7 @@ Route::middleware("jwt")->group(function () {
         ], 404);
     });
 
-    // for invoicea
+    // for invoices
     Route::apiResource("invoices", InvoicesController::class, ["as" => "api"])->missing(function () {
         return response()->json([
             "status" => false,
@@ -56,4 +57,7 @@ Route::middleware("jwt")->group(function () {
             ], 404);
         })->name("api.histories.show");
     });
+
+    // for dashboard
+    Route::get("/dashboard", DashboardController::class)->name("api.dashboard");
 });
